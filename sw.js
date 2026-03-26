2f7197012382ca7e54abcbd790306f2b3f9f5506
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-03-26 08:00:34 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const cacheName = 'vibras-positivas-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});

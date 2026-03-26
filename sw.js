ce2225b09492f760b6b814243d0bb597b51bd283
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-03-26 08:08:22 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
 const cacheName = 'vibras-positivas-v1';
 const assets = [
+  './',
   'index.html',
   'manifest.json',
+  'icon-512.png',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching de recursos');
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
